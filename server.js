a92e7a4bd1b0e838cb8d8bfd2d553959cefa9607
Hami Ansari <hamiansari265@gmail.com>
2025-10-16 11:54:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
 const express = require('express');
+const path = require('path');
+require('dotenv').config();
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
+});const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
